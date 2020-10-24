07772b15facebf4dd5dc68eb6042a8d0728f3a43
Tofu Gang <tofugangsw@gmail.com>
2020-10-24 10:48:54 +0200

    Initial commit.

---
+++ AdventOfCode2018.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2018
+{
+    class AdventOfCode2018
+    {
+        static void Main(string[] args)
+        {
+            Day01.Day01.Puzzle1();
+        }
+    }
+}
